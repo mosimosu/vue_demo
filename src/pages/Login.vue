db9fabf5d98f063f37c7b519817ffea32bb00756
Mosi <mosisu07@gmail.com>
2024-11-12 01:38:32 +0800

    feat: create login function

---
+++ src/pages/Login.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <h1>Login</h1>
+  <label for="email">
+    Email
+    <input type="text" name="email" id="email" v-model="email">
+  </label>
+  <label for="password">
+    Password
+    <input type="password" name="password" id="password" v-model="password">
+  </label>
+  <button @click="login">Login</button>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import {apiBatch, postToAPI} from '../api/apiV1.js';
+
+const email = ref('');
+const password = ref('');
+const login = async () => {
+  const res = await postToAPI(apiBatch.login, {
+    user:{
+      email: email.value,
+      password: password.value
+    }
+  });
+  console.log(res.message);
+}
+
+</script>
+
+
+
+<style scoped>
+
+</style>
