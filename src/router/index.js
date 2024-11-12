d706dd176cd1cf8e3f3fe7681799ec92ab9ec0c4
Mosi <mosisu07@gmail.com>
2024-11-13 01:27:11 +0800

    Chore: added route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import SignUp from '../pages/SignUp.vue'
 import Login from '../pages/Login.vue'
+import Home from '../pages/Home.vue'
+
 const routes = [
     { path: '/', component: SignUp },
-    { path: '/login', component: Login },]
+    { path: '/login', component: Login },
+    { path: '/home', component: Home },
+]
 
+/**
+ * router instance
+ * @type {Router}
+ */
  const router = createRouter({
     history: createWebHistory(),
     routes  // short for `routes: routes`
