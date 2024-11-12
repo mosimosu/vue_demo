4963abb0c3fe7621c68fd4310bd57855400074ea
Mosi <mosisu07@gmail.com>
2024-11-13 01:28:59 +0800

    Feature: added get function

--- src/api/apiV1.js
+++ src/api/apiV1.js
@@ -11,23 +11,50 @@ export const apiBatch = {
 
 /**
  * 傳送post請求
- * @param url {String || Object}
+ * @param url {String}
+ * @param token {String || null}
  * @param data {Object}
  * @return {Promise<*>}
  */
-export async function postToAPI(url, data) {
+export async function postToAPI(url,token = null, data) {
     try {
         // 使用 fetch 的第二個參數為物件，其中 method 為請求方法，headers 為請求的標頭，body 為請求的內容
         const response = await fetch(url, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization':  token ? `Bearer ${token.split(' ')[1]}` : ''
             },
             body: JSON.stringify(data)
         });
 
-        // 將 response 轉為 json 格式並回傳
-        return await response.json();
+        // 將 response回傳
+        return await response;
+    } catch (error) {
+        // 將錯誤回傳
+        return error;
+    }
+}
+
+/**
+ * 傳送get請求
+ * @param url {String || Object}
+ * @param headers {Object}
+ * @return {Promise<*>}
+ */
+export async function getToAPI(url,headers) {
+    try {
+        // 使用 fetch 的第二個參數為物件，其中 method 為請求方法
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${headers.split(' ')[1]}`
+            }
+        });
+
+        // 將 response 回傳
+        return await response;
     } catch (error) {
         // 將錯誤回傳
         return error;
