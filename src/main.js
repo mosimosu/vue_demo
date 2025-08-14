7752f5e28e3797b71952833dd816cb1ffa6b3d5e
Mosi <mosisu07@gmail.com>
2025-08-14 16:52:37 +0800

    Chore: integrate Vuetify into the application

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(router, vuetify).mount('#app')
