3d4a961feee95fdf591a22f30c6f4d7b25a21155
Mosi <mosisu07@gmail.com>
2025-08-15 17:08:12 +0800

    Feat: integrate TanStack Query for enhanced data fetching capabilities

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import {createApp} from 'vue'
 // Vuetify
 import vuetify from './plugins/vuetify'
 
+// TanStack Query
+import {VueQueryPlugin} from "@tanstack/vue-query";
+import {queryClient} from './api/queryClient.js'
+
 import App from './App.vue'
 import router from './router'
 
 
-createApp(App).use(router).use(vuetify).mount('#app')
+createApp(App).use(router).use(vuetify).use(VueQueryPlugin, {queryClient}).mount('#app')
