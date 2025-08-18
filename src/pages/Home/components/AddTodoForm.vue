502896b54f1bff5d02cee205e640ecd37402eea3
Mosi <mosisu07@gmail.com>
2025-08-18 17:17:13 +0800

    Refactor: add AddTodoForm component for creating new todos

---
+++ src/pages/Home/components/AddTodoForm.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {provide, ref} from 'vue';
+
+const todo = ref('');
+const emit = defineEmits(['add']);
+
+const submit = () => {
+  if (todo?.value?.trim()) {
+    emit('add', todo.value);
+    todo.value = '';
+  }
+}
+provide('hello', 'hello world!')
+</script>
+
+<template>
+  <v-form @submit.prevent="submit">
+    <v-row align="center">
+      <v-col>
+        <v-text-field
+            label="Add a new todo"
+            v-model="todo"
+            outlined
+        />
+      </v-col>
+      <v-col cols="auto">
+        <v-btn color="primary" type="submit">Add</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
