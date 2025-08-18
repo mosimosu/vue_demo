50f81c959a30fed3f759b797a960b9c37d3f0fc7
Mosi <mosisu07@gmail.com>
2025-08-18 19:22:16 +0800

    Refactor: implement authentication guard in router for protected routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,33 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import {createWebHistory, createRouter} from 'vue-router'
 import Signup from '../pages/SignUp/index.vue'
 import Login from '../pages/Login/index.vue'
 import Home from '../pages/Home/index.vue'
 
 const routes = [
-    { path: '/', component: Signup },
-    { path: '/login', component: Login },
-    { path: '/home', component: Home },
+    {path: '/', component: Signup},
+    {path: '/login', component: Login},
+    {path: '/home', component: Home},
 ]
 
 /**
  * router instance
  * @type {Router}
  */
- const router = createRouter({
+const router = createRouter({
     history: createWebHistory(),
     routes  // short for `routes: routes`
+})
+
+const isAuthenticated = () => {
+    return !!sessionStorage.getItem('token');
+}
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/home' && !isAuthenticated()) {
+        next('/login')
+    } else {
+        next()
+    }
 })
 
 export default router
