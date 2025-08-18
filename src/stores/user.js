0bf1d89ce86acd922bb92857672e78c3ce274490
Mosi <mosisu07@gmail.com>
2025-08-18 21:09:02 +0800

    Feat: implement user store with nickname management and logout functionality

---
+++ src/stores/user.js
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+    state: () => ({
+        nickname: sessionStorage.getItem('user') || ''
+    }),
+    actions: {
+        setNickname(name) {
+            this.nickname = name
+            sessionStorage.setItem('user', JSON.stringify(name))
+        },
+        logout() {
+            this.nickname = ''
+            sessionStorage.clear()
+        }
+    }
+})
